0041f1b271b40775f9f8455835b864d99081e8eb
geekmdtravis <travis@geekmd.io>
2025-05-07 22:18:56 -0700

    added ability to read symbol defs with keystroke

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,10 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>tf", vim.diagnostic.open_float, { noremap = true, silent = true, desc = "open floating diagnostic" })
 map("n", "<leader>lr", vim.lsp.buf.references, { noremap = true, silent = true, desc = "see references to symbol" })
+-- Map a key to show hover documentation
+map(
+  "n",
+  "<leader>k",
+  "<cmd>lua vim.lsp.buf.hover()<CR>",
+  { noremap = true, silent = true, desc = "see hover for symbol" }
+)
